933bf285124869a7480d174907c348e59590bedb
Kallikrates <kallikrates.koios@gmail.com>
2015-03-21 03:10:59 -0500

    Branch-switch checkpoint, nothing really added.

--- codemp/game/bg_phys.cpp
+++ codemp/game/bg_phys.cpp
@@ -2,3 +2,21 @@ extern "C" {
 #include "bg_phys.h"
 }
 #include <btBulletDynamicsCommon.h>
+
+static btBroadphaseInterface* broadphase;
+
+void BG_InitializeSimulation() {
+	if (!broadphase) broadphase = new btDbvtBroadphase();
+}
+
+void BG_ShutdownSimulation() {
+	if(broadphase) { delete broadphase; broadphase = nullptr; }
+}
+
+void BG_RunSimulation() {
+
+}
+
+void BG_RegisterBPhysEntity(entityState_t * ent) {
+
+}
